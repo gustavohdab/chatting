0c060b962dc76ac4fc83119ca810b147686659c8
Gustavo Batista <gustavo.batista@bluecore.it>
2023-08-31 21:13:26 -0300

    feat: :sparkles: conversations setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from 'lucide-react'
 import MobileToggle from '../mobile-toggle'
+import UserAvatar from '../user-avatar'
 
 interface ChatHeaderProps {
   serverId: string
@@ -8,12 +9,15 @@ interface ChatHeaderProps {
   imageUrl?: string
 }
 
-const ChatHeader = ({ serverId, name, type }: ChatHeaderProps) => {
+const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="flex h-12 items-center border-b-2 border-neutral-200 px-3 font-semibold dark:border-neutral-800">
       <MobileToggle serverId={serverId} />
       {type === 'channel' && (
         <Hash className="mr-2 h-5 w-5 text-zinc-500 dark:text-zinc-400" />
+      )}
+      {type === 'conversation' && (
+        <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="font-semibold text-black dark:text-white">{name}</p>
     </div>
