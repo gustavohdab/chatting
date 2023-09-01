dd3a0c6df3a333840a9a94aad0ec63045569bb45
Gustavo Batista <gustavo.batista@bluecore.it>
2023-09-01 16:27:47 -0300

    feat:

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from 'lucide-react'
 import MobileToggle from '../mobile-toggle'
 import UserAvatar from '../user-avatar'
+import { SocketIndicator } from '../socket-indicator'
 
 interface ChatHeaderProps {
   serverId: string
@@ -20,6 +21,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <UserAvatar src={imageUrl} className="mr-2 h-8 w-8 md:h-8 md:w-8" />
       )}
       <p className="font-semibold text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   )
 }
