fa6f65053a42aeb9598ba152c15fabdf980d64da
Gustavo Batista <gustavo.batista@bluecore.it>
2023-09-03 07:16:19 -0300

    fix: :fire: remove unused webpack config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-import path from 'path'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname)
-    return config
-  },
   images: {
     domains: ['uploadthing.com', 'img.clerk.com'],
   },
